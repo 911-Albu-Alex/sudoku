43617a3eb23c6f22f5d5e6b31ad2ea8df8d81eba
Alex <alexmhh1234@gmail.com>
2021-06-23 13:13:55 +0300

    started gui;issues with numpy arrays

--- ui/gui.py
+++ ui/gui.py
@@ -24,12 +24,25 @@ class GUI:
     def __init__(self, service):
         pygame.display.set_caption("Welcome to Sudoku!")
         monitor_info = pygame.display.Info()
-        self._screen = pygame.display.set_mode((monitor_info.current_w, monitor_info.current_h))
+        # self._screen = pygame.display.set_mode((monitor_info.current_w, monitor_info.current_h))
+        self._screen = pygame.display.set_mode((1600, 900))
         self._service = service
+
+    def _convert_board(self, board):
+        for row in range(0, len(board)):
+            for column in range(0, len(board)):
+                if board[row][column] == " ":
+                    board[row][column] = 0
+                else:
+                    board[row][column] = int(board[row][column])
+
+        return board
 
     def draw_board(self):
         board = self._service.get_board()
+        board = self._convert_board(board)
         board = numpy.matrix(board)
+        # board = numpy.fromstring(*board, dtype=int, sep=' ')
         print(board)
         # new_array = numpy.random.randint(3, size=(100, 100))
         surface = pygame.surfarray.make_surface(board)
